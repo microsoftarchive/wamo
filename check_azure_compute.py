068826d0696f970484b76af423f7a416bf0d2e64
Jeff Mendoza <jemendoz@microsoft.com>
2013-04-23 22:16:26 -0700

    Update to take hostname to check, improve output.

--- check_azure_compute.py
+++ check_azure_compute.py
@@ -3,61 +3,56 @@
 import argparse
 import os
 import sys
+import azure
 from azure.servicemanagement import ServiceManagementService
 from azuremonitor.publishsettings import PublishSettings
 
 
 def handle_args():
-    parser = argparse.ArgumentParser(description='Check Azure.',
+    parser = argparse.ArgumentParser(description='Check Azure Compute',
                                      epilog='(c) MS Open Tech')
+    parser.add_argument('hostname', help='hosted service to check')
     parser.add_argument(
         '-p', '--publish-settings',
         required=True,
         help='.publishsettings file to authenticate with azure',
         dest='psfile')
-    parser.add_argument('-v', '--verbose', action='count', default=0)
+    parser.add_argument('-v', '--verbose', action='count', 
+                        default=0, help='verbosity')
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
     return parser.parse_args()
 
 
-def check_for_errors(service):
-    hosted_services = service.list_hosted_services()
+def check_for_errors(service, hostname):
+    #hosted_services = service.list_hosted_services()
     errors = []
-    for service in hosted_services:
-        hosted_service = service.get_hosted_service_properties(
-            service.service_name, embed_detail=True)
-        for deployment in hosted_service.deployments:
-            if deployment.status != 'Running':
-                errors.append(
-                    'Hosted Service {}, Deployment {}, Status: {}'.format(
-                        hosted_service.service_name, deployment.name,
-                        deployment.status))
-            for role_inst in deployment.role_instance_list:
-                if role_inst.power_state != 'Started':
-                    errors.append(
-                        'Hosted Service {}, Deployment {}, Role {}, Power: {}'.format(
-                            hosted_service.service_name, deployment.name,
-                            role_inst.role_name, role_inst.power_state))
-                if role_inst.instance_status != 'ReadyRole':
-                    errors.append(
-                        'Hosted Service {}, Deployment {}, Role {}, Status: {}'.format(
-                            hosted_service.service_name, deployment.name,
-                            role_inst.role_name, role_inst.instance_status))
+    try:
+        service = service.get_hosted_service_properties(
+            hostname,
+            embed_detail=True)
+    except azure.WindowsAzureMissingResourceError, error:
+        errors.append('Hosted service {0} not found'.format(hostname))
+        return errors
+    if service.hosted_service_properties.status != 'Created':
+        errors.append('Service status: {0}'.format(service.hosted_service_properties.status))
+    if not service.deployments:
+        errors.append('No deployments found')
+    for deployment in service.deployments:
+        if deployment.status != 'Running':
+            errors.append('Deployment status: {0}'.format(deployment.status))
+        if not deployment.role_instance_list:
+            errors.append('No role instances found')
+        for role_inst in deployment.role_instance_list:
+            if role_inst.power_state != 'Started':
+                errors.append('Power state: {0}'.format(role_inst.power_state))
+            if role_inst.instance_status != 'ReadyRole':
+                errors.append('Role status: {0}'.format(role_inst.instance_status))
     return errors
 
 
 def print_errors(errors, verbosity):
     if errors:
-        if verbosity == 0:
-            print 'Errors!'
-        else:
-            if len(errors) == 1:
-                print errors[0]
-            else:
-                print 'Multiple errors'
-                if verbosity >= 2:
-                    for error in errors:
-                        print error
+        print ', '.join(errors)
         return 2
     else:
         print 'All cool'
@@ -83,7 +78,7 @@ def main():
     service = ServiceManagementService(
         subscription_id=publishsettings.sub_id,
         cert_file=pem_path)
-    errors = check_for_errors(service)
+    errors = check_for_errors(service, args.hostname)
     if args.verbose >= 3:
         print 'Azure status retreived.'
 
